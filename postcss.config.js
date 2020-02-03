2dd79dc125a9497ac5a1b1e9c14e7da24cddf56d
Roman Ershov <romma4893@gmail.com>
2020-02-03 10:00:01 +0300

    postcss

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+/*module.exports = {
+   plugins: [
+      require('autoprefixer')
+   ]  'postcss-import': {},
+      'postcss-preset-env': {},
+      'cssnano': {}
+}*/
+
+module.exports = {
+   parser: 'sugarss',
+   plugins: {
+
+   }
+}
